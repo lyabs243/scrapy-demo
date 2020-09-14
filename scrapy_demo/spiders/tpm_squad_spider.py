421671346e33fd3c61a61ca0d6e796a11d3414b8
lyabs243 <loicyabili@outlook.com>
2020-09-14 20:13:50 +0200

    Get stats data of players

--- scrapy_demo/spiders/tpm_squad_spider.py
+++ scrapy_demo/spiders/tpm_squad_spider.py
@@ -10,7 +10,53 @@ class TPSquadSpider(scrapy.Spider):
         yield from response.follow_all(player_links, callback=self.parse_squad)
         
     def parse_squad(self, response):
-        
+        statistics = []
+        seasons = response.css('.statsjoueur thead th::text').getall()
+        for i, season in enumerate(seasons):
+            if i != 0 and i < len(seasons)-1:
+                statistics.append({'season': season})
+
+        statistics = self.get_stats(response, statistics)        
+        details = self.get_details(response)
+        yield {
+            'image': response.css('.img-fluid::attr(src)').get(),
+            'details': details,
+            'number': response.css('h2 span::text').get(),
+            'name': response.css('h2::text').get(),
+            'clubs': response.css('.clubs p').get(),
+            'palmares': response.css('.palmares::text').get(),
+            'statistics': statistics
+        }
+
+    def get_stats(self, response, statistics):
+        for attribute in response.css('.statsjoueur tbody tr'):
+            attrName = attribute.css('strong::text').get()
+            statType = ''
+            if 'matchs' in str(attrName):
+                statType = 'played'
+            elif 'Titulaire' in str(attrName):
+                statType = 'standing'
+            elif 'Remplaçant' in str(attrName):
+                statType = 'subtitute'
+            elif 'Minutes' in str(attrName):
+                statType = 'minutes'
+            elif 'buts ' in str(attrName):
+                statType = 'goals'
+            elif 'Passes ' in str(attrName):
+                statType = 'assists'
+            elif 'Jaunes' in str(attrName):
+                statType = 'yellow_card'
+            elif 'Rouges' in str(attrName):
+                statType = 'red_card'
+            
+            if (len(statType) > 0):
+                for index, value in enumerate(attribute.css('.statschiffres::text').getall()):
+                    if index < len(statistics):
+                        statistics[index][statType] = value
+
+        return statistics
+
+    def get_details(self, response):
         player_details = response.css('.data-fiche li')
         for detail in player_details:
             img = detail.css('img::attr(src)').get()
@@ -27,18 +73,11 @@ class TPSquadSpider(scrapy.Spider):
             elif 'poids' in img:
                 weight = detail.css('strong::text').get()
 
-        yield {
-            'image': response.css('.img-fluid::attr(src)').get(),
-            'details': {
+        return {
                 'position': position,
                 'birth': birth,
                 'nationality': nationality,
                 'since': since,
                 'size': size,
                 'weight': weight
-            },
-            'number': response.css('h2 span::text').get(),
-            'name': response.css('h2::text').get(),
-            'clubs': response.css('.clubs p').get(),
-            'palmares': response.css('.palmares::text').get()
-        }
+            }
