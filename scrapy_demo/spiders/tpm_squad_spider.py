898be2e4992d114dd45fd04cee9839298ffa4e6a
lyabs243 <loicyabili@outlook.com>
2020-09-14 14:22:58 +0200

    Add extraction of players details like weight, birth date, nationality...

--- scrapy_demo/spiders/tpm_squad_spider.py
+++ scrapy_demo/spiders/tpm_squad_spider.py
@@ -10,8 +10,33 @@ class TPSquadSpider(scrapy.Spider):
         yield from response.follow_all(player_links, callback=self.parse_squad)
         
     def parse_squad(self, response):
+        
+        player_details = response.css('.data-fiche li')
+        for detail in player_details:
+            img = detail.css('img::attr(src)').get()
+            if 'poste' in img:
+                position = detail.css('strong::text').get()
+            elif 'naissance' in img:
+                birth = detail.css('strong::text').get()
+            elif 'nationalite' in img:
+                nationality = detail.css('strong::text').get()
+            elif 'clubdepuis' in img:
+                since = detail.css('strong::text').get()
+            elif 'taille' in img:
+                size = detail.css('strong::text').get()
+            elif 'poids' in img:
+                weight = detail.css('strong::text').get()
+
         yield {
             'image': response.css('.img-fluid::attr(src)').get(),
+            'details': {
+                'position': position,
+                'birth': birth,
+                'nationality': nationality,
+                'since': since,
+                'size': size,
+                'weight': weight
+            },
             'number': response.css('h2 span::text').get(),
             'name': response.css('h2::text').get(),
             'clubs': response.css('.clubs p').get(),
